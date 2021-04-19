22e86f216bf21737afc4bd59e63a70365828baec
Mateusz Szymoński <mateusz.szymonski@gmail.com>
2021-04-19 20:35:03 +0200

    fix: docker-compose

---
+++ evoting-backend-app/evoting-backend-app-test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using evoting_backend_app.sevices.VotersService;
+
+namespace evoting_backend_app_test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
