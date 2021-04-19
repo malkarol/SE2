22e9e550999574e37d8980e83e1398e03c584ea7
Mateusz Szymoński <mateusz.szymonski@gmail.com>
2021-04-19 23:49:14 +0200

    feat: Add testing project

--- evoting-backend-app/evoting-backend-app/ConfigDatabase/EVotingDatabaseSettings.cs
+++ evoting-backend-app/evoting-backend-app/ConfigDatabase/EVotingDatabaseSettings.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace evoting_backend_app.Models
+namespace evoting_backend_app
 {
     public interface IEVotingDatabaseSettings
     {    
