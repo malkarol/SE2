ec5ec4d8657bfac864fd468cfe7a6b9744d97136
Mateusz Szymoński <mateusz.szymonski@gmail.com>
2021-04-27 12:32:31 +0200

    feat: Voter service GetVoter

--- evoting-backend-app/evoting-backend-app/Services/VotersService.cs
+++ evoting-backend-app/evoting-backend-app/Services/VotersService.cs
@@ -10,16 +10,58 @@ namespace evoting_backend_app.Services
 {
     public class VotersService
     {
-        private readonly IMongoCollection<Voter> voters;
+        private readonly IMongoDatabase mainDatabase;
+        private readonly IMongoDatabase votesDatabase;
+        private readonly IMongoDatabase registrationRequestsDatabase;
+
+        private readonly IMongoCollection<Voter> votersCollection;
+        private readonly IMongoCollection<Voting> votingsCollection;
+
 
         public VotersService(IEVotingDatabaseSettings settings)
         {
             var client = new MongoClient(settings.ConnectionString);
-            var mainDatabase = client.GetDatabase(settings.MainDatabaseName);
 
-            voters = mainDatabase.GetCollection<Voter>(settings.VotersCollectionName);
+            // Get databases
+            mainDatabase = client.GetDatabase(settings.MainDatabaseName);
+            votesDatabase = client.GetDatabase(settings.VotesDatabaseName);
+            registrationRequestsDatabase = client.GetDatabase(settings.RegistrationRequestsDatabaseName);
+
+            // Get collections
+            votersCollection = mainDatabase.GetCollection<Voter>(settings.VotersCollectionName);
+            votingsCollection = mainDatabase.GetCollection<Voting>(settings.VotingsCollectionName);
         }
 
-      
+        private IMongoCollection<Vote> GetVotesCollection(string votingId)
+        {
+            return votesDatabase.GetCollection<Vote>(votingId);
+        }
+
+        private IMongoCollection<RegistrationRequest> GetRegistrationRequestsCollection(string votingId)
+        {
+            return registrationRequestsDatabase.GetCollection<RegistrationRequest>(votingId);
+        }
+
+        // ---
+
+        public async Task<Voter_BasicInfo_DTO> GetVoter(string id)
+        {
+            FilterDefinition<Voter> filter = Builders<Voter>.Filter.Eq(o => o.Id, id);
+            var voterTask = votersCollection.Find(filter).FirstOrDefaultAsync();
+            await Task.WhenAll(voterTask);
+            var voter = voterTask.Result;
+
+            // Create DTO
+            var result = new Voter_BasicInfo_DTO()
+            {
+                FirstName = voter.FirstName,
+                LastName = voter.LastName,
+                Email = voter.Email
+            };
+
+            return result;
+        }
+
+
     }
 }
