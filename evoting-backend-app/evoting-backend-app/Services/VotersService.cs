00d8f97b688c629d5cb430b06be4a9b828061ed6
Mateusz Szymoński <mateusz.szymonski@gmail.com>
2021-04-27 12:27:09 +0200

    ref: Clear voters controller and service

--- evoting-backend-app/evoting-backend-app/Services/VotersService.cs
+++ evoting-backend-app/evoting-backend-app/Services/VotersService.cs
@@ -20,35 +20,6 @@ namespace evoting_backend_app.Services
             voters = mainDatabase.GetCollection<Voter>(settings.VotersCollectionName);
         }
 
-        public virtual async Task<IEnumerable<Voter>> GetAllVoters()
-        {
-            return await voters.Find(_ => true).ToListAsync();
-        }
-
-        public Task<Voter> GetVoter(string id)
-        {
-            FilterDefinition<Voter> filter = Builders<Voter>.Filter.Eq(m => m.Id, id);
-            return voters.Find(filter).FirstOrDefaultAsync();
-        }
-
-        public async Task CreateVoter(Voter voter)
-        {
-            // ObjectId is generated autmatically if empty Id field is passed
-            await voters.InsertOneAsync(voter);
-        }
-
-        public async Task<bool> UpdateVoter(Voter voter)
-        {
-            ReplaceOneResult updateResult = await voters.ReplaceOneAsync(filter: g => g.Id == voter.Id, replacement: voter);
-            return updateResult.IsAcknowledged && updateResult.ModifiedCount > 0;
-        }
-
-        public async Task<bool> DeleteVoter(string id)
-        {
-            FilterDefinition<Voter> filter = Builders<Voter>.Filter.Eq(m => m.Id, id);
-            DeleteResult deleteResult = await voters.DeleteOneAsync(filter);
-            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
-        }
-
+      
     }
 }
